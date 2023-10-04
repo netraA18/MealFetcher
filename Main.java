9386dcf075a9d3785e864f4054c4e94a59cf4da7
netraA18 <netraamin13@gmail.com>
2023-10-04 15:12:43 -0400

    firstCommit

---
+++ Main.java
@@ -0,0 +1,83 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter a dish: ");
+            String dish = scanner.nextLine();
+            String mealResponse = MealAPIUtil.getMealByName(dish);
+
+            if (mealResponse.startsWith("error")) {
+                System.out.println(mealResponse);
+            } else {
+                // Parse the JSON response
+                /*
+                 * mealObject contains an array of meals, so the code extracts
+                 * this array using mealObject.get("meals") and stores it in the
+                 * mealsArray
+                 */
+                JSONParser parser = new JSONParser();
+                JSONObject mealObject = (JSONObject) parser.parse(mealResponse);
+
+                // Extract and print the meal details
+                /*
+                 * checks if the mealsArray is not null and not empty so like if
+                 * there are meals in the array, it extracts the first meal's details
+                 */
+                JSONArray mealsArray = (JSONArray) mealObject.get("meals");
+                JSONObject meal;
+                for (int i = 0; i < mealsArray.size(); i++) {
+                    if (mealsArray != null && !mealsArray.isEmpty()) {
+                        meal = (JSONObject) mealsArray.get(i);
+                        String mealName = (String) meal.get("strMeal");
+                        String mealCategory = (String) meal.get("strCategory");
+                        String mealInstructions = (String) meal.get("strInstructions");
+
+                        String[] sentences = mealInstructions.split("\\.");
+
+                        System.out.println("Meal Name:\t\t " + mealName);
+                        System.out.println();
+                        System.out.println("Category:\t\t " + mealCategory);
+                        System.out.println();
+                        System.out.println("Instructions:");
+                        System.out.println();
+
+                        for (int j = 1; j < sentences.length; j++) {
+                            System.out.println(j + ") " + sentences[j].trim());
+
+                        }
+                        System.out.println();
+
+                        System.out.println("Measurements and Ingredients:\n ");
+                        for (int m = 1; m <= 20; m++) {
+                            String mealMeasurements = (String) meal.get("strMeasure" + m);
+                            String mealIngredients = (String) meal.get("strIngredient" + m);
+                            if (mealMeasurements != null && !mealMeasurements.trim().isEmpty() && mealIngredients != null && !mealIngredients.trim().isEmpty()) {
+                                System.out.println(" ~ " + mealMeasurements + " of " + mealIngredients);
+                            }
+                        }
+                        System.out.println();
+                        
+
+                    } else {
+                        System.out.println("No meal data found for " + dish);
+                        
+                    }
+
+                }
+
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ MealAPIUtil.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class MealAPIUtil {
+    private static final String API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+    public static String getMealByName(String dish) throws IOException {
+        // Create a URL for the API request
+        URL url = new URL(API_URL + dish);
+
+        // Open a connection to the URL
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        // Read the response
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        } catch (java.net.UnknownHostException e) {
+            System.out.println(e);
+            return "error";
+        } finally {
+            connection.disconnect();
+        }
+    }
+}
