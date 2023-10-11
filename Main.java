db82b7401790a710a7d77e26f178e7be2e31c127
netraA18 <netraamin13@gmail.com>
2023-10-10 23:04:11 -0400

    organized into methods

--- Main.java
+++ Main.java
@@ -4,11 +4,10 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         try {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter a dish: ");
@@ -18,68 +17,62 @@ public class Main {
             if (mealResponse.startsWith("error")) {
                 System.out.println(mealResponse);
             } else {
-                // Parse the JSON response
-                /*
-                 * mealObject contains an array of meals, so the code extracts
-                 * this array using mealObject.get("meals") and stores it in the
-                 * mealsArray
-                 */
-                JSONParser parser = new JSONParser();
-                JSONObject mealObject = (JSONObject) parser.parse(mealResponse);
-
-                // Extract and print the meal details
-                /*
-                 * checks if the mealsArray is not null and not empty so like if
-                 * there are meals in the array, it extracts the first meal's details
-                 */
-                JSONArray mealsArray = (JSONArray) mealObject.get("meals");
-                JSONObject meal;
-                for (int i = 0; i < mealsArray.size(); i++) {
-                    if (mealsArray != null && !mealsArray.isEmpty()) {
-                        meal = (JSONObject) mealsArray.get(i);
-                        String mealName = (String) meal.get("strMeal");
-                        String mealCategory = (String) meal.get("strCategory");
-                        String mealInstructions = (String) meal.get("strInstructions");
-                        String youTube = (String) meal.get("strYoutube");
-
-                        String[] sentences = mealInstructions.split("\\.");
-
-                        System.out.println("Meal Name:\t\t " + mealName + "\n");
-                        System.out.println("Category:\t\t " + mealCategory+ "\n");
-                        System.out.println("Youtube:\t\t " + youTube + "\n");
-                        System.out.println("Instructions:"+ "\n");
-                        
-                        
-
-                        for (int j = 1; j < sentences.length; j++) {
-                            System.out.println(j + ") " + sentences[j].trim());
-
-                        }
-                        System.out.println();
-
-                        System.out.println("Measurements and Ingredients:\n ");
-                        for (int m = 1; m <= 20; m++) {
-                            String mealMeasurements = (String) meal.get("strMeasure" + m);
-                            String mealIngredients = (String) meal.get("strIngredient" + m);
-                            if (mealMeasurements != null && !mealMeasurements.trim().isEmpty() && mealIngredients != null && !mealIngredients.trim().isEmpty()) {
-                                System.out.println(" ~ " + mealMeasurements + " of " + mealIngredients);
-                            }
-                        }
-                        System.out.println();
-
-                        
-                        
-
-                    } else {
-                        System.out.println("No meal data found for " + dish);
-                        
-                    }
-
-                }
-
+                processMealResponse(mealResponse, mealResponse);
             }
-        } catch (IOException | ParseException e) {
+            scanner.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void processMealResponse(String mealResponse, String dish) {
+        JSONParser parser = new JSONParser();
+        JSONObject mealObject;
+        JSONArray mealsArray;
+        try {
+            mealObject = (JSONObject) parser.parse(mealResponse);
+            mealsArray = (JSONArray) mealObject.get("meals");
+        } catch (ParseException e) {
+            System.out.println("Error parsing JSON response.");
+            return;
+        }
+
+        if (mealsArray != null && !mealsArray.isEmpty()) {
+            for (int i = 0; i < mealsArray.size(); i++) {
+                printMealDetails((JSONObject) mealsArray.get(i));
+            }
+        } else {
+            System.out.println("No meal data found for " + dish);
+        }
+    }
+
+    private static void printMealDetails(JSONObject meal) {
+        String mealName = (String) meal.get("strMeal");
+        String mealCategory = (String) meal.get("strCategory");
+        String mealInstructions = (String) meal.get("strInstructions");
+        String youTube = (String) meal.get("strYoutube");
+
+        String[] sentences = mealInstructions.split("\\.");
+
+        System.out.println("Meal Name:\t\t " + mealName + "\n");
+        System.out.println("Category:\t\t " + mealCategory + "\n");
+        System.out.println("Youtube:\t\t " + youTube + "\n");
+        System.out.println("Instructions:" + "\n");
+
+        for (int j = 1; j < sentences.length; j++) {
+            System.out.println(j + ") " + sentences[j].trim());
+        }
+        System.out.println();
+
+        System.out.println("Measurements and Ingredients:\n ");
+        for (int m = 1; m <= 20; m++) {
+            String mealMeasurements = (String) meal.get("strMeasure" + m);
+            String mealIngredients = (String) meal.get("strIngredient" + m);
+            if (mealMeasurements != null && !mealMeasurements.trim().isEmpty()
+                    && mealIngredients != null && !mealIngredients.trim().isEmpty()) {
+                System.out.println(" ~ " + mealMeasurements + " of " + mealIngredients);
+            }
+        }
+        System.out.println();
     }
 }
